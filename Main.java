4845679194f12e7faf2a95e7db10a35f16133105
Brandon Jonathan Brown <61449380+BrandonJonathanBrown@users.noreply.github.com>
2021-07-06 17:05:45 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,33 @@
-
-import com.client.Client_Interface;
-
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.scene.image.Image;
-/**
- * @author Brandon Jonathan Brown
- */
-public class Main extends Application{
+ /**
+  * 
+  * @author Brandon Jonathan Brown
+  *
+  */
+public class Main extends Application {
+ 
+	//Launches the application
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+ 
+    @Override
+    public void start(Stage primaryStage) {
+    	
+        game v = new game();
+    	primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
+        primaryStage.setTitle("Ping Pong Game");
+        primaryStage.setScene(v.scene());
+        primaryStage.centerOnScreen();
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setResizable(false);
+        primaryStage.show();
 
-	//Launches javafx application
-	public static void main(String []args)
-	{
-		Application.launch(args);
-	}
-	
-	//Sets the stage for the javafx application
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-	
-		primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("_Appicon.png")));
-		primaryStage.setScene(new Client_Interface().window());
-		primaryStage.setResizable(false);
-		primaryStage.isFocused();
-		primaryStage.centerOnScreen();
-		primaryStage.initStyle(StageStyle.UTILITY);
-	    primaryStage.setTitle("");
-		primaryStage.show(); 
-	} 
-	
+    }
+
+  
+
 }
